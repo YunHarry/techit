1de845cb1fb49bbf2f9c72e848b0fd4a00490efd
Harry <harry.backend@gmail.com>
2023-09-01 15:32:09 +0900

    POST method updated

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def hello_world(request):
+    if request.method == "POST":
+        temp = request.POST.get('email')
+        return render(request, "accountapp/hello_world.html",
+                      context={"temp": temp})
+
     temp = "techit"
     return render(request, "accountapp/hello_world.html",
                   context={"temp": temp})
