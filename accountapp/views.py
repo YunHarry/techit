e16b88a318d3d601622811df7e9649e17cbf7dfa
Harry <harry.backend@gmail.com>
2023-09-04 13:41:37 +0900

    bootstrap form updated

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
 # Create your views here.
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
@@ -9,6 +8,7 @@ from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from accountapp.models import Registration
+
 
 def hello_world(request):
     if request.method == "POST":
@@ -29,9 +29,9 @@ def hello_world(request):
                   context={"temp": temp,
                            "reg_all": reg_all})
 
+
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
     success_url = reverse_lazy("accountapp:hello_world")
     template_name = "accountapp/create.html"
-
