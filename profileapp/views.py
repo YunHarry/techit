9d03ff7ef559e02a3e772d3e1df7aa6c2db682c7
Harry <harry.backend@gmail.com>
2023-09-06 14:23:37 +0900

    profile update btn updated

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
 from profileapp.forms import ProfileForm
 from profileapp.models import Profile
@@ -16,6 +16,16 @@ class ProfileCreateView(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('accountapp:detail',
+                       kwargs={'pk':self.request.user.pk})
+
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    form_class = ProfileForm
+    context_object_name = 'target_profile'
+    template_name = 'profileapp/update.html'
 
     def get_success_url(self):
         return reverse('accountapp:detail',
