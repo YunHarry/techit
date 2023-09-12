e074e2621247e42747a594fe2e342f6d2805dd78
Harry <harry.backend@gmail.com>
2023-09-12 15:21:15 +0900

    mobile responsive updated

--- techit/settings.py
+++ techit/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = 'django-insecure-b_nh5b666g01hxp9*rd1ulj8$w6ac(2s%fw^w)=39u#&ax7gbn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '*'
+]
 
 
 # Application definition
