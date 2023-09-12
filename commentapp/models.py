4b79d298634e144a763cbfe71c08e9c2aeedfe46
Harry <harry.backend@gmail.com>
2023-09-12 13:46:34 +0900

    CommentUpdateView updated

--- commentapp/models.py
+++ commentapp/models.py
@@ -6,7 +6,9 @@ from articleapp.models import Article
 
 # Create your models here.
 class Comment(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE, null=False)
-    writer = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, null=False,
+                                related_name='comments')
+    writer = models.ForeignKey(User, on_delete=models.CASCADE, null=False,
+                               related_name='comments')
     content = models.TextField(null=False)
     created_at = models.DateTimeField(auto_now_add=True)
